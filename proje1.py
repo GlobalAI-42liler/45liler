4b203ae0d0447513a05d4d379416cc140b19e11a
Walker835 <mustafayvz-3211@hotmail.com>
2022-07-04 23:18:31 +0300

    test update 1

--- proje1.py
+++ proje1.py
@@ -1,21 +1,27 @@
-if(sonuc >=90):
-    print("Harf Notu: A1, Başarılı.")
-elif(sonuc>=85):
-    print("Harf Notu: A2, Başarılı.")
-elif(sonuc>=80):
-    print("Harf Notu: A3,Başarılı.")
-elif(sonuc>=75):
-    print("Harf Notu: B1, Geçer.")
-elif(sonuc>=70):
-    print("Harf Notu: B2, Geçer.")
-elif(sonuc>=65):
-    print("Harf Notu: B3, Koşullu Geçer.")
-elif(sonuc>=60):
-    print("Harf Notu: C1, Koşullu Geçer.")
-elif(sonuc>=55):
-    print("Harf Notu: C2, Koşullu Geçer.")
-elif(sonuc>=50):
-    print("Harf Notu: C3, Koşullu Geçer.")
-else:
-    print("Harf Notu: FF, Kaldı.")
-      
+def not_giriniz():
+    adsoyad = input('İsminiz: ')
+    okulno = input('Okul Numaranız: ')
+    sınavpuani1 = input('1. Sınav Notunuz: ')
+    sınavpuani2 = input('2. Sınav Notunuz: ')
+
+    with open("bilgiler.txt","a", encoding="utf-8") as file:  # yazılan bilgileri txt ye kayıt ettiriyor   #a a Moduyla dosyaya veri kayıt edicez
+        file.write(adsoyad+' '+ okulno+ ' '+sınavpuani1+','+sınavpuani2+'\n') # verileri yazdırıyoruz
+
+def save_al():
+    pass
+def ortalamalar_globalAI():
+    with open("bilgiler.txt","r",encoding="uft-8") as file: #okuma işlemi yapıcaz
+        for satir in file:
+            print(satir)
+
+while True:
+    islem = input('1 - Bilgileri Giriniz\n2 - Notu Kayıt Et\n3 - Notları Görüntüle\n4 - Kapat\n')
+
+    if islem == '1':
+        not_giriniz() #not giririş 
+    elif islem == '2':
+        save_al()     #notları txt ye kayıt ettirecek
+    elif islem == '3':
+        save_al()
+    else:           #yanlış girilen bi numarada fonksiyonu durduracak
+        break
