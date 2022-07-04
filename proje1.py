112e8000eb20979b56cb2640374098520d482bbb
Walker835 <mustafayvz-3211@hotmail.com>
2022-07-04 22:24:41 +0300

    update1

---
+++ proje1.py
@@ -0,0 +1,18 @@
+if(sonuc >=90):
+    print("Harf Notu: A1, Başarılı.")
+elif(sonuc>=85):
+    print("Harf Notu: A2, Başarılı.")
+elif(sonuc>=80):
+    print("Harf Notu: A3,Başarılı.")
+elif(sonuc>=75):
+    print("Harf Notu: B1, Geçer.")
+elif(sonuc>=70):
+    print("Harf Notu: B2, Geçer.")
+elif(sonuc>=65):
+    print("Harf Notu: B3, Koşullu Geçer.")
+elif(sonuc>=60):
+    print("Harf Notu: C1, Koşullu Geçer.")
+elif(sonuc>=55):
+    print("Harf Notu: C2, Koşullu Geçer.")
+elif(sonuc>=50):
+  print("Harf Notu: C3, Koşullu Geçer.")
