607ee51bec96e666177c909b6724212e2cb1e660
Walker835 <mustafayvz-3211@hotmail.com>
2022-07-05 16:01:19 +0300

    update2
    def fonksiyonu nu class olarak değiştirdik
    biraz sohbet sonrasında daha düzgün ve hoş olması için bu kararı aldık

--- proje1.py
+++ proje1.py
@@ -1,21 +1,26 @@
-# from openpyxl import Workbook,load_workbook
-# from openpyxl.utils import get_column_letter
-# from openpyxl.styles import Font
+class puan_hesaplama(satir):
+    satir = satir[:-1]
+    liste = satir.split(' ')
 
+    AdSoyad = liste[0]
+    notlar = liste[1]
+    
+    ortalama = (sınavpuani1+sınavpuani2)/2
 
-def not_giriniz():
+class not_giriniz():
     adsoyad = input('İsminiz: ')
     okulno = input('Okul Numaranız: ')
-    sınavpuani1 = input('1. Sınav Notunuz: ')
-    sınavpuani2 = input('2. Sınav Notunuz: ')
+    sinavpuani1 = input('1. Sınav Notunuz: ')
+    sinavpuani2 = input('2. Sınav Notunuz: ')
 
     with open("bilgiler.txt","a", encoding="utf-8") as file:  # yazılan bilgileri txt ye kayıt ettiriyor   #a a Moduyla dosyaya veri kayıt edicez
-        file.write(adsoyad+' '+ okulno+ ' '+sınavpuani1+','+sınavpuani2+'\n') # verileri yazdırıyoruz
+        file.write(adsoyad+' '+ okulno+ ' '+sinavpuani1+','+sinavpuani2+'\n') # verileri yazdırıyoruz
 
-def save_al():
+class save_al():
     pass
-def ortalamalar_globalAI():
-    with open("bilgiler.txt","r",encoding="uft-8") as file: #okuma işlemi yapıcaz
+
+class ortalamalar_globalAI():
+    with open("bilgiler.txt","r",encoding="utf-8") as file: #okuma işlemi yapıcaz
         for satir in file:
             print(satir)
 
@@ -27,62 +32,6 @@ while True:
     elif islem == '2':
         save_al()     #notları txt ye kayıt ettirecek
     elif islem == '3':
-        save_al()
+        ortalamalar_globalAI()
     else:           #yanlış girilen bi numarada fonksiyonu durduracak
         break
-
-
-# data={
-#     'Joe':{
-#     'Math':90,
-#     'History':65,
-#     'Physics':100,
-#     'Geography':55,
-#     },
-#     'Matilda':{
-#     'Math':65,
-#     'History':50,
-#     'Physics':85,
-#     'Geography':75,
-#     },
-
-#      'Jon':{
-#     'Math':100,
-#     'History':55,
-#     'Physics':80,
-#     'Geography':90,
-#     },
-#     'Melda':{
-#     'Math':50,
-#     'History':95,
-#     'Physics':90,
-#     'Geography':55,
-#     },
-#     'Ben':{
-#     'Math':100,
-#     'History':45,
-#     'Physics':85,
-#     'Geography':75,
-#     }
-    
-#     }
-
-
-# wb=Workbook()
-# ws=wb.active
-# ws.title="Grades"
-
-# headings=['Name'] + list(data['Joe'].keys())
-
-# ws.append(headings)
-
-# for person in data:
-#     grades=list(data[person].values())
-#     ws.append([person]+grades)
-# for col in range(2,len(data['Joe'])+2):
-#     char = get_column_letter(col)
-#     ws[char+7]=f"SUM({char +'2'}:{char +'6'})/{len(data)}"
-# for col in range(1,6):
-#     ws[get_column_letter(col)+1].font=Font(bold=True,color="00008080")        
-
-# wb.save('NewGrades.xlsx')   
