538b9e7dc501f1ec81b89cc3ab422e3796087178
furkansi <89041970+furkansi@users.noreply.github.com>
2022-07-05 18:58:45 +0300

    Update proje1.py
    Öğrenci bilgileri defi güncellendi

--- proje1.py
+++ proje1.py
@@ -3,32 +3,40 @@ from logging.config import listen
 
 Data = []
 
-def puan_hesaplama(sinavpuani1,sinavpuani2):
+def ogrenciBilgiler():
+  adsoyad = input("Adınız ve Soyadınız: ")
+  okulno = input("Okul Numaranız: ")
+  ders = input("Ders giriniz: ")
+  vize = int(input("1. sınav puanınız:"))
+  final = int(input("2. sınav puanınız:"))
 
-    ortalama = sinavpuani1*0.4+sinavpuani2*0.6
+  ortalama = (vize*0.4+final*0.6)
 
-    if ortalama >100:
-        harf="AA"
-        passing = "Gecti"
-    elif ortalama >85:
-        harf="BA"
-        passing = "Gecti"
-    elif ortalama >65:
-        harf="CC"
-        passing = "Kaldi"
-    else:
-        harf="FF"
-        passing = "Kaldi"
+  if ortalama >=90 and ortalama <= 100:
+    harf = "A1 Başarılı"
+  elif ortalama >= 85 and ortalama < 90:
+    harf = "A2 Başarılı"
+  elif ortalama >= 80 and ortalama < 85:
+    harf = "A3 Başarılı"
+  elif ortalama >= 75 and ortalama < 80:
+    harf = "B1 Geçti"
+  elif ortalama >= 70 and ortalama < 75:
+    harf = "B2 Geçti"
+  elif ortalama >= 65 and ortalama < 70:
+    harf = "B3 Geçti"
+  elif ortalama >= 60 and ortalama < 65:
+    harf = "C1 Koşullu Geçti"
+  elif ortalama >= 55 and ortalama < 60:
+    harf = "C2 Koşullu Geçti"
+  elif ortalama >= 50 and ortalama < 65:
+    harf = "C3 Koşullu Geçti"
+  else:
+    harf="FF Kaldı"
+  print(f"{okulno} numaralı {adsoyad} adlı öğrencinin {ders} dersinden, {ortalama} ortalama ile aldığı harf notu {harf}")
+ogrenciBilgiler()
 
-    return harf
-    
-def not_giriniz():
-    adsoyad = input('İsminiz: ')
-    okulno = input('Okul Numaranız: ')
-    sinavpuani1 = int(input('1. Sınav Notunuz: '))
-    sinavpuani2 = int(input('2. Sınav Notunuz: '))
-    Data.append([adsoyad,okulno,sinavpuani1,sinavpuani2,(sinavpuani1*0.4+sinavpuani2*0.6),puan_hesaplama(sinavpuani1,sinavpuani2)])
-    print(Data)
+   # Data.append([adsoyad,okulno,sinavpuani1,sinavpuani2,(sinavpuani1*0.4+sinavpuani2*0.6),puan_hesaplama(sinavpuani1,sinavpuani2)])
+   #print(Data)
 
 
 
