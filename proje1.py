bd573b316846950635f8ca978b28218c8fd47b6a
Walker835 <mustafayvz-3211@hotmail.com>
2022-07-05 16:49:00 +0300

    test

--- proje1.py
+++ proje1.py
@@ -5,11 +5,11 @@ def puan_hesaplama(satir):
     ogrenciAdi = liste[0]
     puanlar = liste[1].split(',')
 
-    sinavpuani1 = int(puanlar[0])
-    sinavpuani2 = int(puanlar[1])
-    
-    ortalama = (sinavpuani1+sinavpuani2)/2
-    
+    sınavpuani1 = int(puanlar[0])
+    sınavpuani2 = int(puanlar[1])
+
+    ortalama = (sınavpuani1+sınavpuani2)/2
+
     if ortalama>=90 and ortalama<=100:
         harf="AA"
     elif ortalama>=85 and ortalama<=89:
@@ -22,31 +22,32 @@ def puan_hesaplama(satir):
     return ogrenciAdi + ": " + harf + "\n" 
 
 
-    def not_giriniz():
-        adsoyad = input('İsminiz: ')
-        okulno = input('Okul Numaranız: ')
-        sinavpuani1 = input('1. Sınav Notunuz: ')
-        sinavpuani2 = input('2. Sınav Notunuz: ')
+def not_giriniz():
+    adsoyad = input('İsminiz: ')
+    okulno = input('Okul Numaranız: ')
+    sınavpuani1 = input('1. Sınav Notunuz: ')
+    sınavpuani2 = input('2. Sınav Notunuz: ')
 
-        with open("bilgiler.txt","a", encoding="utf-8") as file:  # yazılan bilgileri txt ye kayıt ettiriyor   #a a Moduyla dosyaya veri kayıt edicez
-            file.write(adsoyad+' '+ okulno+ ' '+sinavpuani1+','+sinavpuani2+'\n') # verileri yazdırıyoruz
+    with open("bilgiler.txt","a", encoding="utf-8") as file:  # yazılan bilgileri txt ye kayıt ettiriyor   #a a Moduyla dosyaya veri kayıt edicez
+        file.write(adsoyad+' '+ okulno+ ' '+sınavpuani1+','+sınavpuani2+'\n') # verileri yazdırıyoruz
 
-    def save_al():
-        pass
+def save_al():
+    pass
 
-    def ortalamalar_globalAI():
-        with open("bilgiler.txt","r",encoding="utf-8") as file: #okuma işlemi yapıcaz
-            for satir in file:
-                print(puan_hesaplama(satir))
 
-    while True:
-        islem = input('1 - Bilgileri Giriniz\n2 - Notu Kayıt Et\n3 - Notları Görüntüle\n4 - Kapat\n')
+def ortalamalar_globalAI():
+    with open("bilgiler.txt","r",encoding="uft-8") as file: #okuma işlemi yapıcaz
+        for satir in file:
+            print(puan_hesaplama(satir))
 
-        if islem == '1':
-            not_giriniz() #not giriş 
-        elif islem == '2':
-            save_al()     #notları txt ye kayıt ettirecek
-        elif islem == '3':
-            ortalamalar_globalAI()
-        else:           #yanlış girilen bi numarada fonksiyonu durduracak
-            break
+while True:
+    islem = input('1 - Bilgileri Giriniz\n2 - Notu Kayıt Et\n3 - Notları Görüntüle\n4 - Kapat\n')
+
+    if islem == '1':
+        not_giriniz() #not giririş 
+    elif islem == '2':
+        save_al()     #notları txt ye kayıt ettirecek
+    elif islem == '3':
+        save_al()
+    else:           #yanlış girilen bi numarada fonksiyonu durduracak
+        break
